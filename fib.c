77cf7a381168f6f3180f621c48f796e80991e18d
Joshua <Jrusli@csu.fullerton.edu>
2024-09-24 18:41:43 -0700

    done

--- fib.c
+++ fib.c
@@ -2,82 +2,65 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Function prototypes with memoization array passed as a parameter
-unsigned long long recursive(int n, unsigned long long *memo);
-unsigned long long iterative(int n, unsigned long long *memo);
+unsigned long long recursive(int input);
+unsigned long long iterative(int input);
 
 int main(int argc, char *argv[]) {
-
-    if (argc != 3) {
+    // Check if the correct number of arguments is passed
+    if (argc != 3) 
+    {
         fprintf(stderr, "Usage: %s <input_num> <method>\n", argv[0]);
         return 1;
     }
 
-    int n = atoi(argv[1]);
+    // Convert the first argument to integer
+    int input_num = atoi(argv[1]);
 
-
-    if (n <= 0) {
-        fprintf(stderr, "Error: Input number must be a positive integer.\n");
-        return 1;
-    }
-
-    unsigned long long *memo = (unsigned long long *)malloc((n + 1) * sizeof(unsigned long long));
-
-    for (int i = 0; i <= n; i++) {
-        memo[i] = -1;
-    }
-
+    // Use the input number directly for Fibonacci calculation
     unsigned long long result;
-   
-    if (strcmp(argv[2], "r") == 0) {
-        result = recursive(n, memo);
-    } else if (strcmp(argv[2], "i") == 0) {
-        result = iterative(n, memo);
-    }
+    if (strcmp(argv[2], "r") == 0) 
+    {
+        result = recursive(input_num);
+    } 
+    else if (strcmp(argv[2], "i") == 0) 
+    {
+        result = iterative(input_num);
+    } 
 
- 
+    // Output the Fibonacci number
     printf("%llu\n", result);
-
-  
 
     return 0;
 }
 
-
-unsigned long long recursive(int n, unsigned long long *memo) {
-    if (n == 1) {
+unsigned long long recursive(int input) {
+    if (input == 1) 
+    {
         return 0;
     }
-    if (n == 2) {
+    if (input == 2)
+    {
         return 1;
     }
-
-
-    if (memo[n] != -1) {
-        return memo[n];
-    }
-
-    memo[n] = recursive(n - 1, memo) + recursive(n - 2, memo);
-    return memo[n];
+    return recursive(input - 1) + recursive(input - 2);
 }
 
-
-unsigned long long iterative(int n, unsigned long long *memo) {
-    if (n == 1) {
+unsigned long long iterative(int input) {
+    if (input == 1) 
+    {
         return 0;
     }
-    if (n == 2) {
+    if (input == 2) 
+    {
         return 1;
     }
-
     unsigned long long a = 0;
     unsigned long long b = 1;
-    for (int i = 3; i <= n; i++) {
+    for (int i = 3; i <= input; i++) 
+    {
         unsigned long long temp = b;
         b = b + a;
         a = temp;
     }
-
-    memo[n] = b;
-    return memo[n];
+    return b;
 }
